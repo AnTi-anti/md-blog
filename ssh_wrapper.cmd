86a5567a2942776ae724a3d08f30714b2c6a39c0
weilei.gakki@gmail.com <weilei.gakki@gmail.com>
2026-04-02 15:28:25 +0800

    publish: site update

---
+++ ssh_wrapper.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Windows\System32\OpenSSH\ssh.exe" -F NUL -i "D:\software\CODEX-~1\document\GITHUB~1\LOCAL-~1\ID_ED2~1" -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=accept-new" %*
